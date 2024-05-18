c38650ab98160dce45be0bfd2674c12d796625b7
ellldar <eldarnyc@gmail.com>
2024-05-18 23:11:20 +0600

    Updates

--- src/events/index.ts
+++ src/events/index.ts
@@ -2,7 +2,10 @@ export * from './listener';
 export * from './publisher';
 export * from './subjects';
 
+export * from './types/order-status';
+
 export * from './ticket-events/ticket-created-event';
 export * from './ticket-events/ticket-updated-event';
 
-export * from './types/order-status';
+export * from './order-events/order-created-event';
+export * from './order-events/order-cancelled-event';
---
+++ src/events/order-events/order-created-event.ts
@@ -0,0 +1,20 @@
+import { Subjects } from "../subjects";
+import { OrderStatus } from "../types/order-status";
+
+export interface OrderCreatedEvent {
+	subject: Subjects.OrderCreated;
+	data: {
+		id: string;
+		status: OrderStatus;
+		userId: string;
+		expiresAt: string;
+		ticket: {
+			id: string;
+			price: number;
+		};
+	};
+}
+
+// The information here is converted to JSON anyways
+// So, it's essential to save dates as String
+// This way the service can consider the timezone
